eb0142d5c70d74b18d3f4f20593ecd5ea53b9983
Alex Damian <alexdamian121@yahoo.com>
2017-12-20 13:44:41 +0200

    Added GameBase skeletal structure

---
+++ src/GameBase.cpp
@@ -0,0 +1,31 @@
+#include "GameBase.h"
+
+GameBase::GameBase() 
+{
+	
+}
+
+GameBase::GameBase(const GameBase& obj) 
+{
+	
+}
+
+GameBase::GameBase(const GameBase && obj)
+{
+
+}
+
+GameBase::~GameBase()
+{
+
+}
+
+GameBase & GameBase::operator=(const GameBase & obj)
+{
+	return *this;
+}
+
+GameBase & GameBase::operator=(const GameBase && obj)
+{
+	return *this;
+}
